a712df1883320f806759a14a3d31e692f74a63af
Ryan Gibbons <Ryan Gibbons insanity5902@gmail.com>
2011-02-13 15:48:40 -0600

    Removed base file and settings, starting ChromaticApp

---
+++ ChromaticApp.h
@@ -0,0 +1,16 @@
+//
+//  ChromaticApp.h
+//  Chromatic
+//
+//  Created by Ryan Gibbons on 2/13/11.
+//  Copyright 2011 Warren Douglas. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface ChromaticApp : NSObject {
+
+}
+
+@end
---
+++ ChromaticApp.m
@@ -0,0 +1,14 @@
+//
+//  ChromaticApp.m
+//  Chromatic
+//
+//  Created by Ryan Gibbons on 2/13/11.
+//  Copyright 2011 Warren Douglas. All rights reserved.
+//
+
+#import "ChromaticApp.h"
+
+
+@implementation ChromaticApp
+
+@end
