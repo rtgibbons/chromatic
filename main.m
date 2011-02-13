991db8cd86eeeb73815648b8ed45a34ba39ef3f6
Ryan Gibbons <Ryan Gibbons insanity5902@gmail.com>
2011-02-13 14:32:35 -0600

    initial Project creation from Xcode

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Chromatic
+//
+//  Created by Ryan Gibbons on 2/13/11.
+//  Copyright 2011 Warren Douglas. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
